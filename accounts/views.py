63c60642da1094359cd32c2ad0d85d27d5d784df
andresperes33 <andresperesdev@gmail.com>
2026-01-28 15:14:13 -0300

    Projeto feito até agora com baseFuções

---
+++ accounts/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+
+
+def register_view(request):
+   if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('login')  # temporário
+   else:
+        user_form = UserCreationForm()
+    
+    # Renderiza o template com o formulário
+   return render(request, 'register.html', {'user_form': user_form})
+
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+            return redirect('cars_list')
+
+    login_form = AuthenticationForm()
+    return render(request, 'login.html', {'login_form': login_form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('cars_list')
+
+
+
+
+
---
+++ cars/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import Car, Brand
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+admin.site.register(Brand, BrandAdmin)
+
+
+
+
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand')
+
+
+admin.site.register(Car, CarAdmin)
+
+
+
